7257c85ac94f82a73ee91a620812648d35e60e53
pouyansh <pshirzadian26@gmail.com>
2019-03-18 09:58:52 +0330

    index app -> news -> model and create view

--- apps/index/urls.py
+++ apps/index/urls.py
@@ -5,4 +5,5 @@ from apps.index.views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('add_news/', AddNews.as_view(), name='add_news')
 ]
--- apps/index/views.py
+++ apps/index/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from apps.index.models import *
 
 
 def index(request):
     return render(request, 'index/index.html')
+
+
+class AddNews(CreateView):
+    model = News
+    template_name = 'index/add_news.html'
+
+    fields = ['title', 'summary', 'description', 'english_title', 'english_summary', 'english_description', 'image']
+    success_url = reverse_lazy('index.index')
+
+    def get_queryset(self):
+        return super(AddNews, self).get_queryset()
