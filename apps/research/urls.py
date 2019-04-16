0a936858d3ba01087be8bd2655920f7265c93eab
pouyansh <pshirzadian26@gmail.com>
2019-04-16 15:15:28 +0430

    apps -> research -> update research area

--- apps/research/urls.py
+++ apps/research/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
 from apps.research.views import *
 
 urlpatterns = [
     path('create_research_area/', AddResearchArea.as_view(), name='add_research_area'),
+    url('update_research_area/(?P<pk>[0-9]+)/$', staff_member_required(UpdateResearchArea.as_view()),
+        name='update_research_area'),
 ]
--- apps/research/views.py
+++ apps/research/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
 from apps.product.models import Category
 from apps.research.forms import AddResearchAreaForm
@@ -12,6 +11,21 @@ class AddResearchArea(CreateView):
     model = ResearchArea
     template_name = 'research/add_research_area.html'
 
+    form_class = AddResearchAreaForm
+    success_url = reverse_lazy('index:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_categories'] = Category.objects.all().order_by('id')
+        context['services'] = Service.objects.all().order_by('id')
+        context['service_fields'] = Field.objects.all().order_by('id')
+        context['research_areas'] = ResearchArea.objects.all().order_by('id')
+        return context
+
+
+class UpdateResearchArea(UpdateView):
+    model = ResearchArea
+    template_name = 'research/update_research_area.html'
     form_class = AddResearchAreaForm
     success_url = reverse_lazy('index:index')
 
