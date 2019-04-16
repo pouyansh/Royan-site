30423cf80650661338f32dd03ab649d77f69535f
pouyansh <pshirzadian26@gmail.com>
2019-04-16 16:23:11 +0430

    apps -> tutorial -> Tutorial list to admin and delete and update

--- apps/tutorial/views.py
+++ apps/tutorial/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, UpdateView, DetailView
+from django.views.generic import CreateView, UpdateView, DetailView, ListView, FormView
 
 from apps.product.models import Category
 from apps.research.models import ResearchArea
@@ -59,4 +59,26 @@ class ShowTutorialDetail(DetailView):
         if not Tutorial.objects.filter(id=self.kwargs['pk']):
             return redirect('index:index')
         return super().dispatch(request, *args, **kwargs)
+
+
+class ShowResearchAreaListAdmin(ListView, FormView):
+    model = Tutorial
+    template_name = 'tutorial/show_tutorial_list_admin.html'
+
+    success_url = reverse_lazy('tutorial:show_tutorial_list_admin')
+    form_class = TutorialListAdminForm
+
+    def form_valid(self, form):
+        Tutorial.objects.filter(id=form.cleaned_data['tutorial_id']).delete()
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_categories'] = Category.objects.all().order_by('id')
+        context['services'] = Service.objects.all().order_by('id')
+        context['service_fields'] = Field.objects.all().order_by('id')
+        context['research_areas'] = ResearchArea.objects.all().order_by('id')
+        context['tutorials'] = Tutorial.objects.all().order_by('id')
+        return context
+
 
