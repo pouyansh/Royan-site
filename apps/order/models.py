47d6beac8022f9555def811fa42f01ed2e6ff1c4
pouyansh <pshirzadian26@gmail.com>
2019-04-25 14:40:23 +0430

    apps -> order -> model Order created

--- apps/order/models.py
+++ apps/order/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from apps.registration.models import Customer
+from apps.service.models import Service
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete="Cascade", verbose_name="مشتری")
+    service = models.ForeignKey(Service, on_delete="DoNothing", verbose_name="سرویس")
+    file = models.FileField(verbose_name="فایل")
+    is_finished = models.BooleanField(default=False)
