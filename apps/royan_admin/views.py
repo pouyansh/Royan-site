810523f7ee02bda917e088b17150ddaca9104d08
pouyansh <pshirzadian26@gmail.com>
2019-05-01 11:27:06 +0430

    apps -> royan_admin -> show messages

--- apps/royan_admin/views.py
+++ apps/royan_admin/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 
+from apps.message.models import Message
 from apps.order_service.models import OrderService
 from apps.product.models import Category
 from apps.research.models import ResearchArea
@@ -19,9 +20,12 @@ class AdminPanel(TemplateView):
         context['research_areas'] = ResearchArea.objects.all().order_by('id')
         context['tutorials'] = Tutorial.objects.all().order_by('id')
         context['finished_orders'] = OrderService.objects.filter(is_finished=True, invoice=True, received=True,
-                                                                 payed=True)
-        context['orders_not_payed'] = OrderService.objects.filter(is_finished=True, invoice=True, payed=False)
-        context['orders_not_received'] = OrderService.objects.filter(is_finished=True, invoice=True, received=False)
-        context['orders_not_invoice'] = OrderService.objects.filter(is_finished=True, invoice=False)
-        context['orders_not_finished'] = OrderService.objects.filter(is_finished=False)
+                                                                 payed=True).order_by('id')
+        context['orders_not_payed'] = OrderService.objects.filter(is_finished=True, invoice=True, payed=False).order_by(
+            'id')
+        context['orders_not_received'] = OrderService.objects.filter(is_finished=True, invoice=True,
+                                                                     received=False).order_by('id')
+        context['orders_not_invoice'] = OrderService.objects.filter(is_finished=True, invoice=False).order_by('id')
+        context['orders_not_finished'] = OrderService.objects.filter(is_finished=False).order_by('id')
+        context['messages'] = Message.objects.all().order_by('-id')
         return context
