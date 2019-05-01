5e87af243b2cd527767e77945313c9a95d07a9ef
pouyansh <pshirzadian26@gmail.com>
2019-05-01 09:57:48 +0430

    apps -> message -> chat button

--- apps/message/urls.py
+++ apps/message/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from apps.message.views import *
 
 urlpatterns = [
-    url('create_message/', CustomerCreateMessage.as_view(), name='create_message'),
+    url('create_message/', CustomerCreateMessage.as_view(), name='customer_create_message'),
 ]
