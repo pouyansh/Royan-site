2706fc631233fe5414a54b18971f624b3f0841e5
pouyansh <pshirzadian26@gmail.com>
2019-05-01 10:15:10 +0430

    apps -> message -> admin create message

--- apps/message/urls.py
+++ apps/message/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.contrib.admin.views.decorators import staff_member_required
 from django.urls import path
 
 from apps.message.views import *
 
 urlpatterns = [
-    url('create_message/', CustomerCreateMessage.as_view(), name='customer_create_message'),
+    path('create_message/', CustomerCreateMessage.as_view(), name='customer_create_message'),
+    url(r'create_message/admin/(?P<pk>[0-9]+)/', staff_member_required(AdminCreateMessage.as_view()),
+        name="admin_create_message"),
 ]
--- apps/message/views.py
+++ apps/message/views.py
@@ -29,3 +29,25 @@ class CustomerCreateMessage(CreateView):
         form.instance.customer = Customer.objects.get(username=self.request.user.username)
         form.instance.is_sender = True
         return super(CustomerCreateMessage, self).form_valid(form)
+
+
+class AdminCreateMessage(CreateView):
+    model = Message
+    form_class = CreateMessageForm
+    template_name = "message/create_message_admin.html"
+    success_url = reverse_lazy("royan_admin:admin_panel")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_categories'] = Category.objects.filter(is_active=True).order_by('id')
+        context['services'] = Service.objects.all().order_by('id')
+        context['service_fields'] = Field.objects.all().order_by('id')
+        context['research_areas'] = ResearchArea.objects.all().order_by('id')
+        context['tutorials'] = Tutorial.objects.all().order_by('id')
+        context['customer'] = Customer.objects.get(id=self.kwargs['pk'])
+        return context
+
+    def form_valid(self, form):
+        form.instance.customer = Customer.objects.get(id=self.kwargs['pk'])
+        form.instance.is_sender = False
+        return super(AdminCreateMessage, self).form_valid(form)
