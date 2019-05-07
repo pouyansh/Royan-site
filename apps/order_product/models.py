d77d26bcc40dfa1c5001201c2600cea063260fac
pouyansh <pshirzadian26@gmail.com>
2019-05-07 17:57:37 +0430

    apps -> order_product -> model OrderProduct

--- apps/order_product/models.py
+++ apps/order_product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django_jalali.db import models as jmodels
 
-# Create your models here.
+from apps.product.models import Product
+from apps.registration.models import Customer
+
+
+class OrderProduct(models.Model):
+    customer = models.ForeignKey(Customer, verbose_name="مشتری", on_delete="Cascade")
+    product = models.ForeignKey(Product, verbose_name="کالا", on_delete="Cascade")
+    price = models.IntegerField(verbose_name="قیمت")
+    description = models.TextField(max_length=1000, verbose_name="توضیحات اضافه")
+    received = models.BooleanField(default=False, verbose_name="آیا تحویل داده شده است؟")
+    invoice = models.BooleanField(default=False, verbose_name="آیا پیش فاکتور ارسال شده است؟")
+    payed = models.BooleanField(default=False, verbose_name="آیا پرداخت شده است؟")
+    date = jmodels.jDateField(verbose_name="تاریخ", blank=True, null=True)
---
+++ apps/order_product/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from django.contrib.admin.views.decorators import staff_member_required
+
+from apps.order_service.views import *
+
+urlpatterns = [
+]
