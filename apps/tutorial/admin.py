f6b47e7fba470eeff6bebd3e7260da4868f63e4c
pouyansh <pouyan.shirzadian@yahoo.com>
2019-09-02 19:58:07 +0430

    workshops and tutorials

--- apps/tutorial/admin.py
+++ apps/tutorial/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from apps.tutorial.models import Tutorial
+from apps.tutorial.models import Tutorial, Links
 
 admin.site.register(Tutorial)
+admin.site.register(Links)
