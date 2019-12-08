6044de2ac2a017ffd9e6f04fda0ca41a371337fc
pouyan shirzadian <pshirzadian26@gmail.com>
2019-12-08 10:31:48 +0330

    about

--- apps/message/urls.py
+++ apps/message/urls.py
@@ -6,7 +6,7 @@ from apps.message.views import *
 
 urlpatterns = [
     path('message/send/', CustomerCreateMessage.as_view(), name='customer_create_message'),
-    url(r'message/send/admin/(?P<username>\w+)/', staff_member_required(AdminCreateMessage.as_view()),
+    url(r'message/send/admin/(?P<username>\.+)/', staff_member_required(AdminCreateMessage.as_view()),
         name="admin_create_message"),
-    url(r'message/details/(?P<pk>\w+)/', MessageDetails.as_view(), name="message_details")
+    url(r'message/details/(?P<pk>\.+)/', MessageDetails.as_view(), name="message_details")
 ]
--- apps/order_service/urls.py
+++ apps/order_service/urls.py
@@ -4,13 +4,13 @@ from django.contrib.admin.views.decorators import staff_member_required
 from apps.order_service.views import *
 
 urlpatterns = [
-    url(r'order/service/start/(?P<pk>\w+)/', StartOrderService.as_view(), name='start_order'),
-    url(r'order/service/order/(?P<pk>\w+)/', SubmitOrderService.as_view(), name='order_service'),
-    url(r'order/service/check_data/(?P<pk>\w+)/', CheckData.as_view(), name='check_data'),
-    url(r'order/service/get_code/(?P<pk>\w+)/', GetCode.as_view(), name='get_code'),
-    url(r'order/service/details/(?P<pk>\w+)/', OrderDetails.as_view(), name='order_details'),
-    url(r'order/service/received/(?P<pk>\w+)/', CheckReceived.as_view(), name='order_received'),
-    url(r'order/service/payed/(?P<pk>\w+)/', staff_member_required(CheckPayed.as_view()), name='order_payed'),
-    url(r'order/service/set_invoice/(?P<pk>\w+)/', staff_member_required(SetInvoice.as_view()), name='set_invoice'),
-    url(r'order/service/invoice/(?P<pk>\w+)/', staff_member_required(CheckInvoice.as_view()), name='order_invoice')
+    url(r'order/service/start/(?P<pk>\.+)/', StartOrderService.as_view(), name='start_order'),
+    url(r'order/service/order/(?P<pk>\.+)/', SubmitOrderService.as_view(), name='order_service'),
+    url(r'order/service/check_data/(?P<pk>\.+)/', CheckData.as_view(), name='check_data'),
+    url(r'order/service/get_code/(?P<pk>\.+)/', GetCode.as_view(), name='get_code'),
+    url(r'order/service/details/(?P<pk>\.+)/', OrderDetails.as_view(), name='order_details'),
+    url(r'order/service/received/(?P<pk>\.+)/', CheckReceived.as_view(), name='order_received'),
+    url(r'order/service/payed/(?P<pk>\.+)/', staff_member_required(CheckPayed.as_view()), name='order_payed'),
+    url(r'order/service/set_invoice/(?P<pk>\.+)/', staff_member_required(SetInvoice.as_view()), name='set_invoice'),
+    url(r'order/service/invoice/(?P<pk>\.+)/', staff_member_required(CheckInvoice.as_view()), name='order_invoice')
 ]
