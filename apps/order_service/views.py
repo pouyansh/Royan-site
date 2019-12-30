e1a51f824c1dea3f6195f8f1b3cd9b44a708a9a3
pouyan shirzadian <pshirzadian26@gmail.com>
2019-12-30 17:01:39 +0330

    order service -> reading only from files

--- apps/order_service/views.py
+++ apps/order_service/views.py
@@ -154,68 +154,68 @@ class SubmitOrderService(LoginRequiredMixin, FormView):
         # if str(from.cleaned_data['final']) == '1'
         order_type = form.cleaned_data['type']
         data = []
-        if '2' in order_type:
-            for row in content_data:
-                data.append(row)
-        if '1' in order_type:
-            content = form.cleaned_data['file'].read()
-            book = xlrd.open_workbook(file_contents=content)
-            sheet = book.sheet_by_index(0)
-            field_names = ""
-            index = 1
-            while True:
-                if not sheet.cell_value(2, index):
-                    break
-                field_names += sheet.cell_value(2, index) + ";"
-                index += 1
-            if field_names != service.field_names:
-                return reverse_lazy(
-                    'order_service:file_error'
-                    , kwargs={
-                        'text':
-                            "ستون های این فایل تغییر کرده اند و امکان آپلود این فایل وجود ندارد."})
-            field_names = field_names.split(';')
-            row_index = 3
-            while True:
-                row = []
-                check = False
-                for j in range(1, sheet.ncols):
-                    if sheet.cell_value(row_index, j):
-                        if field_names[j - 1] in number_constraints:
-                            try:
-                                float(sheet.cell_value(row_index, j))
-                            except ValueError or TypeError:
+        # if '2' in order_type:
+        #     for row in content_data:
+        #         data.append(row)
+        # if '1' in order_type:
+        content = form.cleaned_data['file'].read()
+        book = xlrd.open_workbook(file_contents=content)
+        sheet = book.sheet_by_index(0)
+        field_names = ""
+        index = 1
+        while True:
+            if not sheet.cell_value(2, index):
+                break
+            field_names += sheet.cell_value(2, index) + ";"
+            index += 1
+        if field_names != service.field_names:
+            return reverse_lazy(
+                'order_service:file_error'
+                , kwargs={
+                    'text':
+                        "ستون های این فایل تغییر کرده اند و امکان آپلود این فایل وجود ندارد."})
+        field_names = field_names.split(';')
+        row_index = 3
+        while True:
+            row = []
+            check = False
+            for j in range(1, sheet.ncols):
+                if sheet.cell_value(row_index, j):
+                    if field_names[j - 1] in number_constraints:
+                        try:
+                            float(sheet.cell_value(row_index, j))
+                        except ValueError or TypeError:
+                            return reverse_lazy(
+                                'order_service:file_error'
+                                , kwargs={
+                                    'text':
+                                        "در ستون " + field_names[j - 1] + " تنها مقادیر عددی مجاز است."})
+                    if field_names[j - 1] in oligo_constraints:
+                        for c in sheet.cell_value(row_index, j):
+                            if c not in oligo_letters:
                                 return reverse_lazy(
                                     'order_service:file_error'
                                     , kwargs={
                                         'text':
-                                            "در ستون " + field_names[j - 1] + " تنها مقادیر عددی مجاز است."})
-                        if field_names[j - 1] in oligo_constraints:
-                            for c in sheet.cell_value(row_index, j):
-                                if c not in oligo_letters:
-                                    return reverse_lazy(
-                                        'order_service:file_error'
-                                        , kwargs={
-                                            'text':
-                                                "در ستون " + field_names[j - 1] + " تنها حروف" + oligo_letters +
-                                                " مجاز است."})
-                        row.append(sheet.cell_value(row_index, j))
-                        check = True
-                if not check:
-                    break
-                if len(row) != len(field_names):
-                    return reverse_lazy(
-                        'order_service:file_error'
-                        , kwargs={
-                            'text':
-                                "فایل شامل سطر هایی ناقص است." +
-                                " لطفا در تمام سطرهایی که اطلاعات پر می کنید، تمامی بخش ها را پر کنید."})
-                row_index += 1
-                data.append(row)
-            with open(order.file.path, 'w') as f:
-                writer = csv.writer(f)
-                for row in data:
-                    writer.writerow(row)
+                                            "در ستون " + field_names[j - 1] + " تنها حروف" + oligo_letters +
+                                            " مجاز است."})
+                    row.append(sheet.cell_value(row_index, j))
+                    check = True
+            if not check:
+                break
+            if len(row) != len(field_names):
+                return reverse_lazy(
+                    'order_service:file_error'
+                    , kwargs={
+                        'text':
+                            "فایل شامل سطر هایی ناقص است." +
+                            " لطفا در تمام سطرهایی که اطلاعات پر می کنید، تمامی بخش ها را پر کنید."})
+            row_index += 1
+            data.append(row)
+        with open(order.file.path, 'w') as f:
+            writer = csv.writer(f)
+            for row in data:
+                writer.writerow(row)
         self.success_url = reverse_lazy('order_service:check_data', kwargs={'pk': self.kwargs['pk']})
         # else:
         #     try:
